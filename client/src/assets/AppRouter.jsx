6403ad5cab7df8668a0b99eea8968401487b10f0
SridharshanY <sridharshany.2001@gmail.com>
2024-12-10 15:23:15 +0530

    completed collapsible navbar

--- client/src/assets/AppRouter.jsx
+++ client/src/assets/AppRouter.jsx
@@ -1,41 +1,41 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import Home from "../pages/Home"
-import About from "../pages/About"
-import SignIn from "../pages/sIGNiN.JSX"
-import SignUp from "../pages/SignUp"
-import Profile from "../pages/Profile"
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import SignIn from "../pages/sIGNiN.JSX";
+import SignUp from "../pages/SignUp";
+import Profile from "../pages/Profile";
+import Header from "../components/Header";
 
 const AppRouter = () => {
-    const router = createBrowserRouter([
-        {
-          path: '/',
-          element: <Home />
-        },
-        {
-          path: '/about',
-          element: <About />
-        },
-        {
-          path: '/signIn',
-          element: <SignIn />
-        },
-        {
-          path: '/signUp',
-          element: <SignUp />
-        },
-        {
-          path: '/profile',
-          element: <Profile />
-        },
-    
-      ])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/about",
+      element: <About />,
+    },
+    {
+      path: "/signIn",
+      element: <SignIn />,
+    },
+    {
+      path: "/signUp",
+      element: <SignUp />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+  ]);
 
-      return (
-        <RouterProvider router={router} />
-      );
-    
-}
+  return (
+    <>
+      <Header />
+      <RouterProvider router={router} />
+    </>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import { FaSearch } from "react-icons/fa";
+import hamburger from "../svg/HAMBURG.svg";
+import { useState } from "react";
+
+const Header = () => {
+  const [ham, setHam] = useState(false);
+
+  return (
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex flex-row justify-between items-center max-w-6xl mx-auto p-3">
+        <h1 className="sm:text-2xl text-sm flex flex-wrap">
+          <span className="text-slate-500">Estate</span>
+          <span className="text-slate-700">Emporium</span>
+        </h1>
+        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="sm:flex sm:gap-4 hidden">
+          <li>Home</li>
+          <li>About</li>
+          <li>Sign In</li>
+        </ul>
+        <button
+          onClick={() => setHam(!ham)}
+          className={`text-black duration-200 block sm:hidden ${
+            ham ? "rotate-180" : "rotate-0"
+          }`}
+        >
+          <img src={hamburger} className="w-8" />
+        </button>
+      </div>
+      <ul className={ham ? `sm:hidden flex flex-col gap-2 p-4 tranition-all duration-300 border border-y-black`: `max-h-0 opacity-0 tranition-all duration-75`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Sign In</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
