12831f89ed5478fec82791c7793e217bad708799
SridharshanY <sridharshany.2001@gmail.com>
2024-12-12 09:41:20 +0530

    started

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://sridharshany2001:paOrwZsRbj1IDD4r@cluster0.so3oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const app = express()
 
